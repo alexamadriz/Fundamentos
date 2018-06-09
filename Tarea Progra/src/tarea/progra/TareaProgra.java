5ec5eebd2a7f7af16358e6682e4b2382c3cf9caf
Alexa Madriz <adma13@hotmail.com>
2018-06-09 12:00:55 -0600

    Tareas

---
+++ Tarea Progra/src/tarea/progra/TareaProgra.java
@@ -0,0 +1,35 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package tarea.progra;
+
+
+import java.util.Scanner;
+
+public class TareaProgra {
+
+ class OddOrEven
+ {
+     
+ }
+    public static void main(String[] args) {
+        
+    }
+    {
+    int x;
+    System.out.println("Enter a number to check if it is odd or even");
+    Scanner in  = new Scanner(System.in);
+    x = in.nextInt();
+    
+    if (x/2 == 0)
+        System.out.println("You entered an even number");
+    else
+        System.out.println("You entered an odd number");
+    }
+        
+    
+    
+    
+}
